92ffd911f7e818787652ec66cee628ae80319692
Purks1337 <Purks@ya.ru>
2026-07-08 01:51:27 +0500

    Configure GitHub Pages deployment via GitHub Actions

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,8 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://Purks1337.github.io',
+  base: '/meyer',
   vite: {
     plugins: [tailwindcss()]
   }
